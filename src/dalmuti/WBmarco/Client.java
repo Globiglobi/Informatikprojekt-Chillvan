339b3582ebede97460c85e1dee5f2f252458f392
GoldiMM <marco.mangold.marco@gmail.com>
2013-11-21 16:42:21 +0100

    Revert "Zusammenspiel von Client und Server mit GUI-Login"
    This reverts commit 1727972e17bca5f56be8c6b23cbd838609641dd5.

--- src/dalmuti/WBmarco/Client.java
+++ src/dalmuti/WBmarco/Client.java
@@ -10,35 +10,32 @@ import java.net.UnknownHostException;
 public class Client {
 	
 	//Attribute
-	static Socket socket;
-	static BufferedReader reader;
-	static OutputStreamWriter writer;
+	Socket socket;
+	BufferedReader reader;
+	OutputStreamWriter writer;
 	
-	public static String messageClient;
-	static String messageServer;
-	
-	public static String userNickname;
+	String messageClient;
+	String messageServer;
 	
 	
 	//Methoden
-	//Methode zum senden Daten
-	public static void send(){
+	//Methode zum "irgendetwas" senden (irgendetwas noch nicht komplett weil man noch nicht weiss was genau senden)
+	public void send(){
 		//try und catch muss sein, da bei Netzwerkkommuniaktion immer etwas schief gehen kann
 		try {
 			//socket, reader, writer erstellen
-			socket = new Socket("127.0.0.1", 50005);
+			socket = new Socket("127.0.0.1", 50000);
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			writer = new OutputStreamWriter(socket.getOutputStream());
+			//"irgendetwas definieren"
 			
 			//message senden vom Client
-			messageClient = userNickname; //Welche message möchte ich senden?
-			writer.write(messageClient);
+			messageClient = " "; //bei " " muss irgendetwas stehen (message die man senden möchte definieren)
+			writer.write(messageClient); 
 			writer.flush();
-			
 			////message lesen vom Server (Was hat der Server zum sagen?)
 			String messageIn = reader.readLine();
-			messageServer = messageIn; 
-			
+			messageServer = messageIn; //statt eine String Bootschaft kann es auch etwas anderes sein
 			//clean up
 			socket.close();
 			reader.close();
@@ -53,13 +50,6 @@ public class Client {
 
 	//Main-Methode
 	public static void main(String[] args) {
-		
-		Login login1 = new Login();
-		userNickname = login1.nickname;
-		
-		Client client1 = new Client();
-		Server server = new Server();
-		
 
 	}
 
