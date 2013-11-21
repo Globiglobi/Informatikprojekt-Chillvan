1727972e17bca5f56be8c6b23cbd838609641dd5
GoldiMM <marco.mangold.marco@gmail.com>
2013-11-21 14:32:06 +0100

    Zusammenspiel von Client und Server mit GUI-Login

--- src/dalmuti/WBmarco/Client.java
+++ src/dalmuti/WBmarco/Client.java
@@ -10,32 +10,35 @@ import java.net.UnknownHostException;
 public class Client {
 	
 	//Attribute
-	Socket socket;
-	BufferedReader reader;
-	OutputStreamWriter writer;
+	static Socket socket;
+	static BufferedReader reader;
+	static OutputStreamWriter writer;
 	
-	String messageClient;
-	String messageServer;
+	public static String messageClient;
+	static String messageServer;
+	
+	public static String userNickname;
 	
 	
 	//Methoden
-	//Methode zum "irgendetwas" senden (irgendetwas noch nicht komplett weil man noch nicht weiss was genau senden)
-	public void send(){
+	//Methode zum senden Daten
+	public static void send(){
 		//try und catch muss sein, da bei Netzwerkkommuniaktion immer etwas schief gehen kann
 		try {
 			//socket, reader, writer erstellen
-			socket = new Socket("127.0.0.1", 50000);
+			socket = new Socket("127.0.0.1", 50005);
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			writer = new OutputStreamWriter(socket.getOutputStream());
-			//"irgendetwas definieren"
 			
 			//message senden vom Client
-			messageClient = " "; //bei " " muss irgendetwas stehen (message die man senden möchte definieren)
-			writer.write(messageClient); 
+			messageClient = userNickname; //Welche message möchte ich senden?
+			writer.write(messageClient);
 			writer.flush();
+			
 			////message lesen vom Server (Was hat der Server zum sagen?)
 			String messageIn = reader.readLine();
-			messageServer = messageIn; //statt eine String Bootschaft kann es auch etwas anderes sein
+			messageServer = messageIn; 
+			
 			//clean up
 			socket.close();
 			reader.close();
@@ -50,6 +53,13 @@ public class Client {
 
 	//Main-Methode
 	public static void main(String[] args) {
+		
+		Login login1 = new Login();
+		userNickname = login1.nickname;
+		
+		Client client1 = new Client();
+		Server server = new Server();
+		
 
 	}
 
