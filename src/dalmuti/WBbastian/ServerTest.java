8244e52d7a64a8ac9709b8ae62ec0804c6d9ecdd
GoldiMM <marco.mangold.marco@gmail.com>
2013-11-26 13:19:27 +0100

    Update

---
+++ src/dalmuti/WBbastian/ServerTest.java
@@ -0,0 +1,11 @@
+package dalmuti.WBbastian;
+
+import javax.swing.JFrame;
+
+public class ServerTest {
+	public static void main(String[] args){
+		Server miu = new Server();
+		miu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		miu.startRunning();
+	}
+}
---
+++ src/dalmuti/WBfabio/Table.java
@@ -0,0 +1,35 @@
+package dalmuti.WBfabio;
+
+public class Table {
+	// Definition der Attribute
+	public String tablename;
+	public int table_ID;
+	public int anzahlUser;
+
+	// Definition der Methode getAnzahlUser
+	public int anzahlUser() {
+		return anzahlUser;
+	}
+
+	// Definition Konstruktor
+	public Table(int table_ID) {
+		this.table_ID = table_ID;
+		
+		//Deklarien der Maximalen Objekte
+		int MAX_OBJ = 2;
+		
+		// Anlegen des Arrays des Objekts einUser
+		Table[] eintable = new Table[MAX_OBJ];
+		
+		//Anlegen der Objekte
+		for (int i = 0; i < MAX_OBJ; i++) {
+			eintable[i] = new Table();
+			}
+
+	}
+
+
+	public Table() {
+		// TODO Auto-generated constructor stub
+	}
+}
