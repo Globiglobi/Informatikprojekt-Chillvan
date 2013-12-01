74e57e5fd16aa24bae02c67d3b418be05c1cfe2a
GoldiMM <marco.mangold.marco@gmail.com>
2013-12-01 10:14:29 +0100

    Update

---
+++ src/beispielLukas/clientServer/MyServerThread.java
@@ -0,0 +1,37 @@
+package beispielLukas.clientServer;
+
+import java.net.*;
+import java.io.*;
+ 
+public class MyServerThread extends Thread {
+    private Socket socket = null;
+ 
+    public MyServerThread(Socket socket) {
+        super("KKMultiServerThread");
+        this.socket = socket;
+    }
+     
+    public void run() {
+ 
+        try (
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                    socket.getInputStream()));
+        ) {
+            String inputLine;
+            String outputLine;
+            MyProtocol protocol = new MyProtocol();
+ 
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = protocol.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye"))
+                    break;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
